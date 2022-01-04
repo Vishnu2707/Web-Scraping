549d9b312ca43c926085fe0109fe0531ee003470
Vishnu Ajith <27vishnu07@gmail.com>
2022-01-05 00:19:11 +0530

    updated

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,8 @@
 import requests                                            #to fetch the url
 from bs4 import BeautifulSoup                              #this is to parse through the data that we fetched.
 
-URL = "https://www.amazon.com/India-Tree-Rainbow-Pepper-Pantry/dp/B07XK81DYY/ref=sr_1_5?adgrpid=1338106518018791&hvadid=83631787970864&hvbmt=be&hvdev=c&hvlocphy=1661&hvnetw=o&hvqmt=e&hvtargid=kwd-83632285155786&hydadcr=24663_13493294&keywords=amazon+india&qid=1641310192&sr=8-5"
+URL = "https://www.worldometers.info/coronavirus/country/india" 
+#URL = "https://www.worldometers.info/coronavirus/"            world data  
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'}
 
@@ -11,6 +12,13 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 soup1 = BeautifulSoup(soup.prettify(), "html.parser")               #formatting the data
 
-title = soup1.find(id="productTitle").get_text()
+date = soup1.find_all('div',style = "font-size:13px; color:#999; text-align:center")
 
-print(title.strip())
+print(date[0].text.strip(),"\n")
+
+cases = soup1.find_all('div', class_= "maincounter-number")
+
+
+print("Corona virus cases :- ",cases[0].text.strip(),"\n")
+print("Deaths :- ",cases[1].text.strip(),"\n")
+print("Recovered :- ",cases[2].text.strip(),"\n")
